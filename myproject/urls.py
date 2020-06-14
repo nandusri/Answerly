114dd07cc31764cb838caf024bdb273022111126
nandini-kasu <nandini.kasu@vtfintech.com>
2020-06-14 13:22:19 +0530

    newtopics list and new topic creation are implemented

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,5 +20,6 @@ from boards import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-
+    path('boards/<pk>/', views.board_topics, name='board_topics'),
+    path('boards/<pk>/new/', views.new_topic, name='new_topic'),
 ]
