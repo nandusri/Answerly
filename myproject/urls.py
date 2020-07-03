c1f2fcc416640960b63853a6466ed1cacf7528f1
nandusri <kasu.nandu@gmail.com>
2020-07-03 19:10:42 +0530

    my account updation implemented

--- myproject/urls.py
+++ myproject/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     #change password
     path('settings/password/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),name='password_change'),
     path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
+    path('settings/account/', accounts_views.UserUpdateView.as_view(), name='my_account'),
 
     path('', views.BoardListView.as_view(), name='home'),
     path('boards/<pk>/', views.TopicListView.as_view(), name='board_topics'),
